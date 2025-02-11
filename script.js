e45cf8dbe3821bb10d2532c1ce6370ce3e3cd092
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-11 11:42:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-//your JS code here. If required.
-class Student {
-	constructor(name) {
-		this.name=name;
-	}
-
-	getkeys(){
-		return Object.keys(this);
-	}
+const student = {
+	name = "John",
+}
+Object.prototype.getKeys = function(){
+	return object.keys(this);
 }
 
-const s =new Student("John")
-console.log(s.getkeys())
+console.log(student.getKeys)
